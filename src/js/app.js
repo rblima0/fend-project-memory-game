5edeb2eb415693385a9b748452340aaf800d6714
Rodrigo Banci <rblima0@gmail.com>
2018-03-02 15:05:17 -0300

    Organizando pastas, codigo e tradução

---
+++ src/js/app.js
@@ -0,0 +1,34 @@
+var cards = [];
+
+
+function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+
+    while (currentIndex !== 0) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+}
+
+
+/*
+ * Crie uma lista que contenha todos os seus cartões
+ * Exibir os cartões na página
+ * - Arraste a lista de cartões usando o método "shuffle" fornecido abaixo
+ * - faça um loop através de cada cartão e crie seu HTML
+ * - adicione o HTML de cada cartão à página
+
+ * configurar o ouvinte de eventos para um cartão. Se um cartão for clicado:
+ * - exiba o símbolo do cartão (coloque esta funcionalidade em outra função que você chama deste)
+ * - adicione o cartão a uma * lista * de cartões "abertos" (coloque esta funcionalidade em outra função que você chama deste)
+ * - se a lista já tiver outro cartão, verifique se as duas cartas correspondem
+ * + se as cartas combinam, bloqueie as cartas na posição aberta (coloque esta funcionalidade em outra função que você chama deste)
+ * + se as cartas não coincidem, remova os cartões da lista e esconda o símbolo do cartão (coloque esta funcionalidade em outra função que você chama deste)
+ * + incrementar o contador de movimentos e exibi-lo na página (coloque esta funcionalidade em outra função que você chama deste)
+ * + se todas as cartas estiverem correspondidas, exiba uma mensagem com a pontuação final (coloque esta funcionalidade em outra função que você chama deste)
+ */
