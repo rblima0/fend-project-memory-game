e9b8df1b85984cfac6442252eecc20cddd156f73
Rodrigo Banci <rblima0@gmail.com>
2018-03-05 15:20:14 -0300

    Alguns ajustes de bugs

--- src/js/app.js
+++ src/js/app.js
@@ -4,6 +4,25 @@ var match = 0;
 var move = 0;
 var star = 0;
 
+var initTimer = false;
+var seconds = 0;
+var minutes = 0;
+var sec = 0;
+
+
+// ADICIONANDO O CARD PELO JS NA PAGINA
+function addCard(card) {
+    $('.deck').append(`<li class="card"><i class="fa ${card}"></i></li>`);
+}
+
+// USANDO A CLASSE SHUFFLE PARA EMBARALHAR AS CARTAS E ADICIONAR NA PAGINA
+function randomCard() {
+    for (var i = 0; i < 2; i++) {
+        gameCards = shuffle(gameCards);
+        gameCards.forEach(addCard);
+    }
+}
+
 // CLASSE PARA RANDOMIZAR AS CARTAS
 function shuffle(array) {
     var currentIndex = array.length, temporaryValue, randomIndex;
@@ -19,33 +38,60 @@ function shuffle(array) {
     return array;
 }
 
-// ADICIONANDO O CARD PELO JS NA PAGINA
-function addCard(card) {
-    $('.deck').append(`<li class="card"><i class="fa ${card}"></i></li>`);
-}
-
-// USANDO A CLASSE SHUFFLE PARA EMBARALHAR AS CARTAS E ADICIONAR NA PAGINA
-function randomCard() {
-    for (var i = 0; i < 2; i++) {
-        gameCards = shuffle(gameCards);
-        gameCards.forEach(addCard);
+// FUNCAO DO TIMER COM MINUTOS E SEGUNDOS
+function startTimer() {
+    if(!initTimer){
+        initTimer = true;
+        function timer(val) { return val > 9 ? val : "0" + val; }
+        this.interval = setInterval(function () {
+            seconds = timer(++sec % 60);
+            minutes = timer(parseInt(sec / 60, 10) % 60);
+            $("#seconds").html(seconds);
+            $("#minutes").html(minutes);
+        }, 1000);
     }
 }
 
-// AÇÕES PARA ADICIONAR AS CLASSES (OPEN, SHOW, ERROR, MATCH) QUANDO OUVER CLIQUE NA CLASSE CARD 
+// FUNÇÃO DE COMBINAÇÃO
+function actionMatch(){
+    match++;
+    $("#match").html(match);
+}
+
+// FUNCAO DOS MOVIMENTOS E DAS ESTRELAS
+function actionMove(){
+    move++;
+    $(".move").html(move);
+
+    if(move <= 12) {
+        star = 3;
+    } else if(move > 12 && move <= 20) {
+        $("#third-star").removeClass("fa-star").addClass("fa-star-o");
+        star = 2;
+    } else if (move > 20 && move <= 28) {
+        $("#second-star").removeClass("fa-star").addClass("fa-star-o");
+        star = 1;
+    } else if (move > 28) {
+        $("#first-star").removeClass("fa-star").addClass("fa-star-o");
+        star = 0;
+    }
+}
+
+// AÇÕES PARA ADICIONAR AS CLASSES (OPEN, SHOW, ERROR, MATCH) QUANDO OUVER CLIQUE NA CLASSE CARD
+selected = false;
 var cardAction = function() {
 	$('.card').on('click', function() {
 
-        moving();
+        startTimer();
         $(this).toggleClass('open show');
-		upsetCards.push($(this));
-
-        if (upsetCards.length >= 2) {
-            if (upsetCards[0][0].firstChild.className == upsetCards[1][0].firstChild.className) {
+        upsetCards.push($(this));
+        
+        if (upsetCards.length == 2) {
+            actionMove();
+            if (upsetCards[0].children().attr("class") === upsetCards[1].children().attr("class")) {
+                actionMatch();
                 $('.open').addClass('match');
                 setTimeout(() => { $('.match').removeClass('open show error'); }, 600);
-                match++;
-                $("#match").html(match);
                 winner();
             } else {
                 $('.open').addClass('error');
@@ -57,38 +103,12 @@ var cardAction = function() {
     });
 }
 
-// FUNCAO DO TIMER COM MINUTOS E SEGUNDOS
-var sec = 0;
-function timer(val) { return val > 9 ? val : "0" + val; }
-setInterval(function () {
-    $("#seconds").html(timer(++sec % 60));
-    $("#minutes").html(timer(parseInt(sec / 60, 10) % 60));
-}, 1000);
-
-// FUNCAO DOS MOVIMENTOS E DAS ESTRELAS
-function moving(){
-    move++;
-    $(".move").html(move);
-
-    if(move <= 24) {
-        star = 3;
-    } else if(move > 24 && move <= 40) {
-        $("#third-star").removeClass("fa-star").addClass("fa-star-o");
-        star = 2;
-    } else if (move > 40 && move <= 56) {
-        $("#second-star").removeClass("fa-star").addClass("fa-star-o");
-        star = 1;
-    } else if (move > 56) {
-        $("#first-star").removeClass("fa-star").addClass("fa-star-o");
-        star = 0;
-    }
-}
-
 // VENCENDO A PARTIDA
 function winner(){
     if(match === 8) {
         setTimeout(() => {
-            alert("Venceu o jogo");
+            alert("Parabens você venceu ! \n" + "Movimentos: " + move + 
+            "\nTempo: " + minutes + " minuto " + seconds + " segundos");
         }, 400);
     }
 }
@@ -115,7 +135,6 @@ initGame();
  * OK - Arraste a lista de cartões usando o método "shuffle" fornecido abaixo
  * OK - faça um loop através de cada cartão e crie seu HTML
  * OK - adicione o HTML de cada cartão à página
-
  * OK - configurar o ouvinte de eventos para um cartão. Se um cartão for clicado:
  * OK - exiba o símbolo do cartão (coloque esta funcionalidade em outra função que você chama deste)
  * - adicione o cartão a uma * lista * de cartões "abertos" (coloque esta funcionalidade em outra função que você chama deste)
